2d659a04ccccb4668f0542f72ac4e0a84e9ecef2
maribel-arac <maribel.arca@gmail.com>
2019-06-09 19:23:17 -0500

    installed react and nav bar created

---
+++ portfolio-react/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './css/style.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul className="nav-menu">
+          <li><a className="nav-menu__link" href="#"> 🏠 Home </a></li>
+          <li><a className="nav-menu__link" href="#"> 😊 About</a></li>
+          <li><a className="nav-menu__link" href="#"> 💼 Projects</a></li>
+          <li><a className="nav-menu__link" href="#"> 🔧 Skills</a></li>
+          <li><a className="nav-menu__link" href="#">📞 Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
