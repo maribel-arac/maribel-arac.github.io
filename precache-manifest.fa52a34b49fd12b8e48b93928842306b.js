02f8ad32bcb7af2410bb9c172525fe7924e99f76
maribel-arac <maribel.arca@gmail.com>
2019-06-13 18:20:55 -0500

    Updates

---
+++ precache-manifest.fa52a34b49fd12b8e48b93928842306b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f48829305af626619ecbe123bb03a1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fafb0552054a73ab886d",
+    "url": "/portfolio/static/css/2.f65ab6ef.chunk.css"
+  },
+  {
+    "revision": "eb30bcc3075e0e0fa501",
+    "url": "/portfolio/static/css/main.da831f68.chunk.css"
+  },
+  {
+    "revision": "fafb0552054a73ab886d",
+    "url": "/portfolio/static/js/2.6cba5eff.chunk.js"
+  },
+  {
+    "revision": "eb30bcc3075e0e0fa501",
+    "url": "/portfolio/static/js/main.fa482b79.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "dd5f06077a545f51befda3b7a4376b6e",
+    "url": "/portfolio/static/media/back-img.dd5f0607.jpg"
+  },
+  {
+    "revision": "2c6060dd038d6b79e316b923e8b72b9d",
+    "url": "/portfolio/static/media/brand.2c6060dd.png"
+  },
+  {
+    "revision": "cf97e4124378a2d63dd977690a96a5f9",
+    "url": "/portfolio/static/media/paws.cf97e412.jpg"
+  },
+  {
+    "revision": "5e639702dbc29ff4143808186f1f8151",
+    "url": "/portfolio/static/media/pokemon.5e639702.jpg"
+  },
+  {
+    "revision": "16546b0cff30c9a0e97328d59a6f7a8f",
+    "url": "/portfolio/static/media/seafood.16546b0c.jpg"
+  }
+]);
