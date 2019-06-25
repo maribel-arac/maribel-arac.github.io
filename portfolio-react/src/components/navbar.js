e4d232d8c97a37d86fb474370a33dea623e7ed23
maribel-arac <maribel.arca@gmail.com>
2019-06-25 12:35:45 -0500

    trying to deploy

--- portfolio-react/src/components/navbar.js
+++ portfolio-react/src/components/navbar.js
@@ -9,7 +9,7 @@ return (
 	
 		<nav className ="navbar-nav navbar-dark text sticky-top  navbar navbar-expand-md">
 		<p className="name">Maribel</p>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<button className="navbar-toggler justify-content-end" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 		    		<span className="navbar-toggler-icon"></span>
 		  		</button> 
 				<div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
