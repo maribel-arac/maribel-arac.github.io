042214840a8925af6cf37efe194a0d0c63eef62d
maribel-arac <maribel.arca@gmail.com>
2019-07-31 12:01:30 -0500

    center text in contact component

--- portfolio-react/src/components/contact.js
+++ portfolio-react/src/components/contact.js
@@ -10,18 +10,18 @@ class Contact extends Component {
 		return(
 			<div  id="contact" className="container  col-lg-12 contactBorder">
 				<div className="row">
-					<div className="col col-12 col-sm-12 col-md-12 col-lg-12 ">
-						<h1 className="contact col-6 col-sm-6 col-md-6 col-lg-6">Contact </h1>
-						<p className="content "> Have a question or want work together?</p>
+					<div className="col-8 col-sm-8 col-md-8 col-lg-8 align-content-center mx-auto">
+						<h1 className="contact col-12 col-sm-12 col-md-12 col-lg-12 text-center">Contact </h1>
+						<p className="content col-12 col-sm-12 col-md-12 col-lg-12 text-center "> Have a question or want work together?</p>
 					</div>
-                        <div className="col  col-sm-6 col-md-4 col-lg-4">
-                            <a target="_blank" href="https://github.com/maribel-arac">
-                               <img src={github} className="logo-btn im-fluid" /> </a>
-                                <h3 className="text-center">GitHub</h3>
+                        <div className="col-8 col-sm-8 col-md-8 col-lg-8 align-self-center mx-auto">
+                            <a target="_blank" className="col-6 col-sm-6 col-md-6 col-lg-6 text-center" href="https://github.com/maribel-arac">
+                               <img src={github} className="logo-git im-fluid mx-auto" /> </a>
+                               
                             
-                            <a target="_blank" href="https://www.linkedin.com/in/maribel-araiza-cañedo/">
-                                <img src={linkedin} className="logo-btn img-fluid" /> </a>
-                                <h3 className="text-center">Linkedin</h3>
+                            <a target="_blank" className="col-6 col-sm-6 col-md-6 col-lg-6 text-center" href="https://www.linkedin.com/in/maribel-araiza-cañedo/">
+                                <img src={linkedin} className="logo-link img-fluid mx-auto" /> </a>
+                                
                         </div>
                 </div>
 
